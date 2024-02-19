9bd561ef6f9343c719bbc602cff378ba788951eb
Donggeun Kim <soonduck3729@gmail.com>
2024-02-19 22:20:08 +0900

    Hello, World!

---
+++ src/test/java/com/example/eliceproject/EliceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eliceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
