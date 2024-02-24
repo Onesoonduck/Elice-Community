9480b66a1a689440ba8adc5947bd4346c587b855
Donggeun Kim <soonduck3729@gmail.com>
2024-02-24 13:08:46 +0900

    feat: postDTO

---
+++ src/main/java/com/example/eliceproject/mapper/PostMapper.java
@@ -0,0 +1,10 @@
+package com.example.eliceproject.mapper;
+
+import com.example.eliceproject.dto.PostDTO;
+import com.example.eliceproject.entity.Post;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PostMapper {
+    Post postDTOToPost(PostDTO postDTO);
+}
