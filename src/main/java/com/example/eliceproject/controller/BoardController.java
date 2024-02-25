21539a888c688bc3c02ca3c73e959ef75114f84d
Donggeun Kim <soonduck3729@gmail.com>
2024-02-25 15:09:14 +0900

    feat: 게시판 키워드 검색 기능  구현

--- src/main/java/com/example/eliceproject/controller/BoardController.java
+++ src/main/java/com/example/eliceproject/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.example.eliceproject.controller;
 
 import com.example.eliceproject.dto.BoardDTO;
 import com.example.eliceproject.entity.Board;
+import com.example.eliceproject.entity.Post;
 import com.example.eliceproject.mapper.BoardMapper;
 import com.example.eliceproject.service.BoardService;
 import com.example.eliceproject.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,20 +37,20 @@ public class BoardController {
     @GetMapping("/main")
     public String main(Model model) {
         model.addAttribute("main", boardService.boardList());
-        return "main";
+        return "/board/main";
     }
 
     // 게시판
     @GetMapping("/board")
     public String boardlist(Model model) {
         model.addAttribute("list", boardService.boardList());
-        return "board";
+        return "/board/board";
     }
 
     // 게시판 생성
     @GetMapping("/board/write")
     public String boardwriteform() {
-        return "boardwrite";
+        return "board/boardwrite";
     }
 
     @PostMapping("/board/writepro")
@@ -60,11 +63,21 @@ public class BoardController {
         return "redirect:/board";
     }
 
-    // 선택한 게시판 보기
-    @GetMapping("/board/{id}")
-    public String getOneBoard(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("boardview", boardService.boardview(id));
-        return "postlist";
+    // 게시판 키워드 검색
+    @GetMapping("/{boardId}")
+    public String getBoard(@PathVariable Integer id,
+                           @RequestParam(defaultValue = "0") int page,
+                           @RequestParam(defaultValue = "10") int size,
+                           @RequestParam(required = false) String keyword,
+                           Model model) {
+        Board board = boardService.findBoardById(id);
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<Post> postPage = postService.findPostsByBoardAndKeyword(board, keyword, pageRequest);
+
+        model.addAttribute("board", board);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("postPage", postPage);
+        return "board/board";
     }
 
     // 게시판 수정
@@ -72,7 +85,7 @@ public class BoardController {
     public String boardModify(@PathVariable("id") Integer id, Model model) {
 
         model.addAttribute("board", boardService.boardview(id));
-        return "boardmodify";
+        return "/board/boardmodify";
     }
 
     @PostMapping("/board/update/{id}")
